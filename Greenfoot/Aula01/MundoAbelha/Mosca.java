2f060a54504825adfff8d8448c2247691535f3a4
kristopher <kristopher.soaresc@gmail.com>
2025-06-04 21:51:37 -0300

    kkk

---
+++ Greenfoot/Aula01/MundoAbelha/Mosca.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Aqui é a Classe Mosca.
+ * 
+ * @author Richard Brosler
+ * @version 2025-06-03
+ */
+public class Mosca extends Actor
+{
+    /**
+     * Act - Método responsável pelas ações da mosca quando
+     * os botões Act ou Run são pressionados.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+        if (Greenfoot.getRandomNumber(100)<20){
+            turn(45-Greenfoot.getRandomNumber(90));
+        }
+    }
+}
